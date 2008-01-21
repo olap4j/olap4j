21877d26f43b987063a913d0710993d5b8612aaf
Julian Hyde <julianhyde@gmail.com>
2008-01-21 14:12:55 +0000

    Remove unused mondrian import
    git-svn-id: https://olap4j.svn.sourceforge.net/svnroot/olap4j/trunk@63 c6a108a4-781c-0410-a6c6-c2d559e19af0

--- src/org/olap4j/impl/ConnectStringParser.java
+++ src/org/olap4j/impl/ConnectStringParser.java
@@ -2,13 +2,11 @@
 // This software is subject to the terms of the Common Public License
 // Agreement, available at the following URL:
 // http://www.opensource.org/licenses/cpl.html.
-// Copyright (C) 2007-2007 Julian Hyde
+// Copyright (C) 2008-2007 Julian Hyde
 // All Rights Reserved.
 // You must accept the terms of that agreement to use this software.
 */
 package org.olap4j.impl;
-
-import mondrian.util.*;
 
 import java.util.*;
 
