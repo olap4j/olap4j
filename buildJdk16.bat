1dd19114fb1c5a410b407e8f72167c5fa76c003b
Julian Hyde <julianhyde@gmail.com>
2009-01-20 23:43:19 +0000

    Create META-INF/services/java.sql.Driver under classes so that driver discovery works in dev environment. Fix ANT_HOME and comments in buildJdk16.bat.
    git-svn-id: https://olap4j.svn.sourceforge.net/svnroot/olap4j/trunk@141 c6a108a4-781c-0410-a6c6-c2d559e19af0

--- buildJdk16.bat
+++ buildJdk16.bat
@@ -1,11 +1,15 @@
-# $Id$
-# Called recursively from 'ant release' to build the files which can only be
-# built under JDK 1.6.
+@echo off
+rem $Id$
+rem Called recursively from 'ant release' to build the files which can only be
+rem built under JDK 1.6.
 
-# Change the following line to point to your JDK 1.6 home.
-set JAVA_HOME=C:\jdk1.6.0_01
+rem Change the following line to point to your JDK 1.6 home.
+set JAVA_HOME=C:\jdk1.6.0_11
+
+rem Change the following line to point to your ant home.
+set ANT_HOME=C:\open\thirdparty\ant
+
 set PATH=%JAVA_HOME%\bin;%PATH%
-ant compile.java
+%ANT_HOME%\bin\ant compile.java
 
 # End buildJdk16.bat
-
