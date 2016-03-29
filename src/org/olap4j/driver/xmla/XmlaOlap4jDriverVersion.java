e9a55d826969e7e9e86116e608b338fd4c92f1c9
Tom Barber <tom@analytical-labs.com>
2016-03-29 23:53:58 +0100

    initial maven prototype

---
+++ src/org/olap4j/driver/xmla/XmlaOlap4jDriverVersion.java
@@ -0,0 +1,12 @@
+package org.olap4j.driver.xmla;
+
+/**
+  * Version information for the XMLA driver. (Generated.)
+  */
+class XmlaOlap4jDriverVersion {
+  static final String NAME = "olap4j driver for XML/A";
+  static final String VERSION = "${project.revision}";
+  static final int MAJOR_VERSION = Integer.valueOf("${project.revision.major}");
+  static final int MINOR_VERSION = Integer.valueOf("${project.revision.minor}");
+
+}
